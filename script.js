9ea5dc952059a2cd39e1fb5373ed8c5f2da0107b
Subash Phuldel <officalsubashphuldel@gmail.com>
2025-01-15 18:41:57 +0545

    improved img-overlay

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const logo = document.querySelector(".logo");
 const navList = document.querySelectorAll(".nav-link");
 
 const animation = gsap.timeline();
+animation.to(".img-overlay", {
+  yPercent: 100,
+  duration: 1.5,
+  stagger: 0.5,
+});
+
 animation.from("img", {
+  opacity: 0,
   scale: 1.2,
   duration: 1,
 });
@@ -12,16 +19,16 @@ animation.from("h1", {
   opacity: 0,
   duration: 1,
 });
-animation.to("main", {
-  y: 64,
-  duration: 1,
-});
 animation.from("h5", {
   y: 100,
   opacity: 0,
   duration: 1,
 });
 
+animation.to("main", {
+  y: 64,
+  duration: 1,
+});
 animation.from(logo, {
   y: -60,
   duration: 1,
